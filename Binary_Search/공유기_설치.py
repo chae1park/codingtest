c51e5902434ff89a3905f56ac4260aab8363d863
chaewon park <tcodnjsdl@gmail.com>
2023-01-31 22:49:45 +0900

    need to work on

---
+++ Binary_Search/공유기_설치.py
@@ -0,0 +1,15 @@
+# 동빈북 p.369 이진 탐색 문제 29 공유기 설치
+
+N, C = 5, 3
+houses = [1, 2, 8, 4, 9]
+output = 3
+
+N, C = 5, 2
+houses = [1, 2, 8, 4, 9]
+output = 8
+
+N, C = 5, 4
+houses = [1, 3, 8, 5, 9] # 1_ 2 5_ 10_ 13 15_ 
+output = 2
+
+print('expected output',output)
